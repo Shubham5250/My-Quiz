e5fa6c593373c3a6ec43cb3cfe81743ff028e562
AnoopVL <avlanjekar4@gmail.com>
2024-02-09 15:01:40 +0530

    Main Activity updated

--- app/src/main/java/com/example/bootcampapp/Screens/Screens/Authentication.java
+++ app/src/main/java/com/example/bootcampapp/Screens/Screens/Authentication.java
@@ -3,6 +3,7 @@ package com.example.bootcampapp.Screens.Screens;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
