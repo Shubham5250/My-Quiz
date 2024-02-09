719d900a7acacc6ce762da1a3d9adbae47f8f345
AnoopVL <avlanjekar4@gmail.com>
2024-02-09 14:47:35 +0530

    Authentication updated

--- app/src/main/java/com/example/bootcampapp/Screens/Screens/Authentication.java
+++ app/src/main/java/com/example/bootcampapp/Screens/Screens/Authentication.java
@@ -3,7 +3,11 @@ package com.example.bootcampapp.Screens.Screens;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import com.example.bootcampapp.R;
 
 
@@ -15,6 +19,17 @@ public class Authentication extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_authentication);
 
-    }
+        Button btnSignup = findViewById(R.id.signUpButton);
 
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Authentication.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
 }
